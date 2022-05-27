aa26dfa16af958093d4f25041e70d821546dfbb1
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-05-27 19:15:05 -0300

    Tentativas reuisitos 8 e 9

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let preto = document.getElementById('black');
-preto.style.backgroundColor = 'black';
+// let preto = document.getElementById('black');
+// preto.style.backgroundColor = 'black';
 
-let vermelho = document.getElementById('red');
-vermelho.style.backgroundColor = 'red';
+// let vermelho = document.getElementById('red');
+// vermelho.style.backgroundColor = 'red';
 
-let azul = document.getElementById('blue');
-azul.style.backgroundColor = 'blue';
+// let azul = document.getElementById('blue');
+// azul.style.backgroundColor = 'blue';
 
-let roxo = document.getElementById('purple');
-roxo.style.backgroundColor = 'purple';
+// let roxo = document.getElementById('purple');
+// roxo.style.backgroundColor = 'purple';
 
+//Requisito 7.
 let palette = document.getElementById('color-palette');
 let color = document.getElementsByClassName('color');
 palette.addEventListener('click', function recebeSelecao (event) {
@@ -20,16 +21,56 @@ palette.addEventListener('click', function recebeSelecao (event) {
   seleciona.classList.add('selected');
 })
 
-function newColor () {
-  let pix = document.getElementsByClassName('pixel');
-  pix.addEventListener('click', function (event) {
-    let receive = event.target;
-    let select = document.querySelector('.selected');
-      receive.style.backgrounColor = select.style.backgrounColor;
-  })
-}
+const element = document.querySelector('.selected');
+const obj = window.getComputedStyle(element, null);
 
-// let vazio = document.getElementsByClassName('pixel');
+let bgColor = obj.getPropertyValue('background-color');
+
+const pixels = document.getElementById('pixel-board');
+
+pixels.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = bgColor;
+})
+
+let button = document.getElementById('clear-board');
+button.addEventListener('click', function () {
+  // let caixa = document.getElementsByClassName('pixel');
+  pixels.style.backgroundColor = 'pink';
+})
+
+// const pi = document.getElementsByClassName('pixel');
+// const button = document.getElementById('clear-board');
+// button.addEventListener('click', function () {
+//   event.target.style.backgroundColor = 'pink';
+// })
+
+// document.getElementById('clear-board').addEventListener('click', function () {
+//   document.querySelector('body').setAttribute.('class', 'white');
+// })
+
+//element.style.backgroundColor
+
+// function again () {
+//   let selec = document.getElementById('pixel-board');
+//   selec.addEventListener('click', function (event) {
+//     let alvo = event.target;
+//     if (alvo.classList.contains('alvo')) {
+//       let targetNow = document.querySelector('.selected');
+//       alvo.style.backgroundColor = targetNow.style.backgroundColor;
+//     }
+//   })
+// }
+
+  // document.getElementById('pixel-board');
+  // let pix = document.getElementById('pixel-board');
+  // pix.addEventListener('click', function () {
+  //   console.log('test');
+  //   let receive = event.target;
+  //   let select = document.querySelector('.selected');
+  //     receive.style.backgrounColor = select.style.backgrounColor;
+  // })
+
+// let vazio = document.getElementById('pixel-board');
 // vazio.addEventListener('click', function (event) {
 //   let recebeCor = event.target;
 //   for (let i = 0; i < vazio.length; i += 1) {
