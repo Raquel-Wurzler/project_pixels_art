7cd8ad89c879e289ef066b4b4fc53bac87482ae6
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-06-05 16:29:48 -0300

    Requisito 10 pronto (yessss)

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ roxo.style.backgroundColor = 'purple';
 //Requisito 7.
 let palette = document.getElementById('color-palette');
 let color = document.getElementsByClassName('color');
-palette.addEventListener('click', function recebeSelecao (event) {
+palette.addEventListener('click', function recebeSelecao(event) {
   let seleciona = event.target;
   for (let index = 0; index < color.length; index += 1) {
     color[index].classList.remove('selected');
@@ -23,41 +23,41 @@ palette.addEventListener('click', function recebeSelecao (event) {
 
 // Requisito 8.
 const pixels = document.getElementById('pixel-board');
-  pixels.addEventListener('click', function (event) {
-    const element = document.querySelector('.selected').style.backgroundColor;
-    let myTarget = event.target.style.backgroundColor;
-        if (element === myTarget) {
-          event.target.style.backgroundColor = 'white';
-        } else {
-          event.target.style.backgroundColor = element;
-        }
-  });
+pixels.addEventListener('click', function (event) {
+  const element = document.querySelector('.selected').style.backgroundColor;
+  let myTarget = event.target.style.backgroundColor;
+  if (element === myTarget) {
+    event.target.style.backgroundColor = 'white';
+  } else {
+    event.target.style.backgroundColor = element;
+  }
+});
 
 //Requisito 9.
 let caixa = document.getElementsByClassName('pixel');
 let button = document.getElementById('clear-board');
 button.addEventListener('click', function () {
   // console.log(caixa);
-  for(let index = 0; index < caixa.length; index += 1) {
+  for (let index = 0; index < caixa.length; index += 1) {
     caixa[index].style.backgroundColor = 'white';
   }
 });
 
 //Requisito 10.
-let paraRemover = document.querySelectorAll('#pixel-board div section')
-
-function clearBoard () {
-  for (let index = 0; index < paraRemover.length; index += 1) {
-    paraRemover[index].remove();
-  }
-}
+function clearBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.innerHTML = '';
+};
 
 const bot = document.getElementById('generate-board');
-
 bot.addEventListener('click', function () {
   let inp = document.getElementById('board-size').value;
-  if (inp === '' || inp < 5 || inp > 50) {
+  if (inp === '') {
     alert('Board inválido!');
+  } else if (inp < 5) {
+    return 5;
+  } else if (inp > 50) {
+    return 50;
   } else {
     console.log(inp);
     clearBoard();
@@ -65,18 +65,26 @@ bot.addEventListener('click', function () {
   }
 });
 
-let teste = 5 ** 2;
-function gerarDivs () {
-  for (let index = 0; index < teste; index += 1) {
-    const div = document.createElement('div');
-    console.log (div);
-    div.className = 'pixel';
-    pixels.appendChild(div);
+function gerarDivs() {
+  let valorRecebido = document.getElementById('board-size').value;
+  for (let linha = 0; linha < valorRecebido; linha += 1) {
+    const divLinha = document.createElement('div');
+    for (let coluna = 0; coluna < valorRecebido; coluna += 1) {
+      const divColuna = document.createElement('div');
+      divColuna.className = 'pixel';
+      divColuna.style.backgroundColor = 'white';
+      divLinha.appendChild(divColuna);
+    }
+    pixels.appendChild(divLinha);
   }
 }
 
+//Requisito 11
+
+
+
 //Requisito 12
-function aleatorio () {
+function aleatorio() {
   const red = parseInt(Math.random() * 255, 10);
   const green = parseInt(Math.random() * 255, 10);
   const blue = parseInt(Math.random() * 255, 10);
